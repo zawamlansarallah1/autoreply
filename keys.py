ab835b894277ad83c522e021798c80b526347be7
zawamlansarallah1 <66381319+zawamlansarallah1@users.noreply.github.com>
2021-06-29 09:56:40 +0200

    Update keys.py

--- keys.py
+++ keys.py
@@ -2,8 +2,8 @@
 #keys.py
 #visit http://dev.twitter.com to create an application and get your keys
 keys = dict(
-	consumer_key='',
-	consumer_secret='',
-	access_token='',
-	access_token_secret='',
+	consumer_key='G0Idfh5BlWs7AsVMXPBP2f5H8',
+	consumer_secret='oqgLL6uQl77p4TTc7ANPH15Y42cSEsZBT0M0EfHFRQ8dyrB06s',
+	access_token='772065071774179328-jVfyJJ9AcvuiMDMFu1uNWnhLt1jlb8g',
+	access_token_secret='BuSSdX4A7W2pZXgtZAapp3b8rct8xb5viJm4n45bowxSf',
 	)
